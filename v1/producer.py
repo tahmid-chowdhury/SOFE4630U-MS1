d813064c8368c54eb0c1a53481810ce27c5739e4
George.Daoud@OntarioTechu.net <GodIsLove@3>
2025-01-09 21:13:01 -0500

    Cloud pub/sub version

--- v1/producer.py
+++ v1/producer.py
@@ -1,7 +1,7 @@
 from google.cloud import pubsub_v1      # pip install google-cloud-pubsub  ##to install
 import glob                             # for searching for json file 
 import json
-import random 
+import os 
 
 # Search the current directory for the JSON file (including the service account key) 
 # to set the GOOGLE_APPLICATION_CREDENTIALS environment variable.
@@ -10,24 +10,28 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=files[0];
 
 # Set the project_id with your project ID
 project_id="";
-topic_name = " testTopic";   # change it for your topic name if needed
+topic_name = "testTopic";   # change it for your topic name if needed
 
 # create a publisher and get the topic path for the publisher
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, topic_name)
-
 print(f"Published messages with ordering keys to {topic_path}.")
 
+#iterate for 100 times 
 for n in range(100):
-    print('Enter a key (String):',end='')
-    record_key = input()
+    # get the value from the user
     print('Enter a value (String):',end='')
     record_value = input()
-    if(record_key<=-1):
+    # stop if empty value is entered
+    if record_value=='':
         break;
+    # convert the string to bytes (serialization)
+    record_value=str(record_value).encode('utf-8')
     
-    print("Producing record: {}\t{}".format(record_key, record_value))    
-    future = publisher.publish(topic_path, data=record_value,ordering_key=record_key);
+    # send the value
+    print("Producing a record: {}".format(record_value))    
+    future = publisher.publish(topic_path, record_value);
     
-    print(future.result())
+    #ensure that the publishing has been completed successfully
+    future.result()
  
