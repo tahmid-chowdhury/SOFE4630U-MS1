1855796a810343d0bca4b0796e3383879af862dd
George.Daoud@OntarioTechu.net <GodIsLove@3>
2025-01-11 17:02:12 -0500

    update the code

--- v2/consumer.py
+++ v2/consumer.py
@@ -22,8 +22,8 @@ print(f"Listening for messages on {subscription_path}..\n")
 
 # A callback function for handling received messages
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    # convert from bytes to string (deserialization)
-    message_data = str(message.data);
+    # convert from bytes to dictionary (deserialization)
+    message_data = json.loads(message.data.decode('utf-8'));
     
     print("Consumed record with value : {}" .format(message_data))
    
